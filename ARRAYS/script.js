0594387ab57016c2547a9bfeaca101ffba5f2594
ENZO GONCALVES SANTOS <enzo.gsantos@senacsp.edu.br>
2024-10-07 17:24:18 -0300

    first commit

---
+++ ARRAYS/script.js
@@ -0,0 +1,96 @@
+/*let fruta = ["abacate", "uva", "banana", "pitaya"]
+console.log(fruta[0])
+fruta.push("manga")
+console.log(fruta[fruta.length -1])//ultimo elemento arrays
+
+for(let i = 0; i < fruta.length; i++) {
+    console.log(fruta[i]);
+}*/
+/*let fruta = ["melão", "melancia", "caju", "laranja"]
+do{
+    console.log(fruta)
+    fruta++;
+}while (fruta <=[4])
+*/
+//let carros = ["Porshe", "Ferrari", "Lambor", "BMW", "Audi"]
+
+//for (let i = 0; i < carros.length; i++) {
+   //console.log(carros[i])
+    
+//}
+// metodo map criar um novo array que contém o comprimento do array carros
+
+
+// let novoCarros = carros.map((carro) => {
+//     return carro.length
+// })
+
+// console.log(novosCarros)//exibir um novo array
+
+//let legumes = ["batata", "cenoura", "beterraba", "berinjela", "chuchu"]
+//let legumesComB = legumes.find(b => b.startsWith("b"))
+//let legumesComB = legumes.filter(legume => legume.startsWith("b"))
+//console.log(legumesComB);
+
+/*function saudacao () {
+    console.log("Olá jovem!");
+}
+
+saudacao();*/
+
+/*function saudacao(nome) {
+    console.log(`Olá ${nome}`);
+}
+saudacao("Joaquim")
+*/
+
+/*function despedida(nome) {
+    console.log(`Até logo ${nome}`);
+}
+despedida("Joaquim");
+
+function soma ( a, b){
+    return a + b;
+}
+console.log(soma(1, 2));
+console.log(soma(4, 9));
+console.log(soma(5, 3));
+
+console.log("--------------")
+
+/*function mult ( a, b){
+    return a * b
+}
+console.log(mult(3, 4))
+console.log(mult(5, 9))
+console.log(mult(6, 7))
+*/
+/*function mult ( a, b){
+    const resultado = a * b;
+    console.log(resultado)
+    return resultado
+}
+mult(1, 2)
+
+let somaAnonima = function(a, b) {
+    return a + b
+}
+
+console.log(somaAnonima(1, 2));*/
+
+//function saudacao(nome) {
+//    console.log();
+//}
+//saudacao("Joaquim")
+
+function saudacao = ["Joaquim"] {
+    console.log(`Olá`saudacao);
+}
+saudacao("Joaquim")
+
+/* Exercicio 2 - function soma( a, b) {
+    return a + b
+}
+console.log(soma (2, 5))
+console.log(soma (9, 7))
+*/
